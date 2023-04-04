7696ff5e1572daa7867f5123704712eb97434e8c
thihamz <minzt154@newschool.edu>
2023-04-04 02:18:08 -0400

    Final Touches

---
+++ munchies/landing.js
@@ -0,0 +1,29 @@
+let imgArr = document.querySelectorAll (".img");
+let prevX = 0;
+let prevY = 0;
+let moveXAmount = 0;
+let moveYAmount = 0;
+
+document.addEventListener("mousemove", function(e){
+    mousePos(e);
+})
+
+function mousePos(e){
+
+    moveXAmount = e.page - prevX;
+    moveYAmount = e.page - prevY;
+    
+    moveImg(moveXAmount, moveYAmount);
+
+    prevX = e.pageX;
+    prevY = e.pageY;
+}
+
+function moveImg( xAmount, yAmount){
+    imgArr .forEach(function(img){
+        let movementStrength = 5 + (Math.random() * 15);
+
+        img.style.left = (img.offsetLeft) - (xAmount/movementStrength) + "px";
+        img.style.top = (img.offsetTop) - (yAmount/movementStrength) + "px";
+    })
+}
